24ce1e479b45612ff207ffb22ba5d704d019ffe3
IsraelMuCa <isra.munoz@gmail.com>
2018-06-08 03:41:51 -0500

    Finished the login functionality and linked it to Firebase, also added the dinamic creation of the fav-row and started the dinamic creation of the modals for the fav categories and subcategories

--- assets/js/create-firebase-categories.js
+++ assets/js/create-firebase-categories.js
@@ -1,61 +1,155 @@
 firebase.database().ref('/categories').child('basketball').set({
-        'subcategories': {
-            1:'NBA',
-            2:'NCAAB'
+    name:'basketball',
+    href:'./assets/img/icons/categories/basketball/basketball.png',
+    'subcategories': {
+        'NBA':{
+            name:'NBA',
+            href:'./assets/img/icons/categories/basketball/NBA/NBA.png'
+        },
+        'NCAAB':{
+            name:'NCAAB',
+            href:'./assets/img/icons/categories/basketball/NCAAB/NCAAB.png'
         }
-    });
-    firebase.database().ref('/categories').child('hockey').set({
-        'subcategories': {
-            1:'NHL'
+    }
+});
+firebase.database().ref('/categories').child('hockey').set({
+    name:'hockey',
+    href:'./assets/img/icons/categories/hockey/hockey.png',
+    'subcategories': {
+        'NHL':{
+            name:'NHL',
+            href:'./assets/img/icons/categories/hockey/NHL/NHL.png'
         }
-    });
-    firebase.database().ref('/categories').child('football').set({
-        'subcategories': {
-            1:'NFL',
-            2:'NCAAF'
+    }
+});
+firebase.database().ref('/categories').child('football').set({
+    name:'football',
+    href:'./assets/img/icons/categories/football/football.png',
+    'subcategories': {
+        'NFL':{
+            name:'NFL',
+            href:'./assets/img/icons/categories/football/NFL/NFL.png'
+        },
+        'NCAAF':{
+            name:'NCAAF',
+            href:'./assets/img/icons/categories/football/NCAAF/NCAAF.png'
         }
-    });
-    firebase.database().ref('/categories').child('golf').set({
-        'subcategories': {
-            1:'PGA'
+    }
+});
+firebase.database().ref('/categories').child('golf').set({
+    name:'golf',
+    href:'./assets/img/icons/categories/golf/golf.png',
+    'subcategories': {
+        'PGA':{
+            name:'PGA',
+            href:'./assets/img/icons/categories/golf/PGA/PGA.png'
         }
-    });
-    firebase.database().ref('/categories').child('mma').set({
-        'subcategories': {
-            1:'UFC'
+    }
+});
+firebase.database().ref('/categories').child('mma').set({
+    name:'mma',
+    href:'./assets/img/icons/categories/mma/mma.png',
+    'subcategories': {
+        'UFC':{
+            name:'UFC',
+            href:'./assets/img/icons/categories/mma/UFC/UFC.png'
         }
-    });
-    firebase.database().ref('/categories').child('boxing').set({
-        'subcategories': {
-            1:'WBA'
+    }
+});
+firebase.database().ref('/categories').child('boxing').set({
+    name:'boxing',
+    href:'./assets/img/icons/categories/boxing/boxing.png',
+    'subcategories': {
+        'WBA':{
+            name:'WBA',
+            href:'./assets/img/icons/categories/boxing/WBA/WBA.png'
         }
-    });
-    firebase.database().ref('/categories').child('tennis').set({
-        'subcategories': {
-            1:'WTA',
-            2:'ATP'
+    }
+});
+firebase.database().ref('/categories').child('tennis').set({
+    name:'tennis',
+    href:'./assets/img/icons/categories/tennis/tennis.png',
+    'subcategories': {
+        'WTA':{
+            name:'WTA',
+            href:'./assets/img/icons/categories/tennis/WTA/WTA.png'
+        },
+        'ATP':{
+            name:'ATP',
+            href:'./assets/img/icons/categories/tennis/ATP/ATP.png'
         }
-    });
-    firebase.database().ref('/categories').child('baseball').set({
-        'subcategories': {
-            1:'MLB',
-            2:'LMP'
+    }
+});
+firebase.database().ref('/categories').child('baseball').set({
+    name:'baseball',
+    href:'./assets/img/icons/categories/baseball/baseball.png',
+    'subcategories': {
+        'MLB':{
+            name:'MLB',
+            href:'./assets/img/icons/categories/baseball/MLB/MLB.png'
+        },
+        'LMP':{
+            name:'LMP',
+            href:'./assets/img/icons/categories/baseball/LMP/LMP.png'
         }
-    });
-    firebase.database().ref('/categories').child('soccer').set({
-        'subcategories': {
-            1:'Fifa World Cup',
-            2:'UEFA Championship League',
-            3:'UEFA Europa League',
-            4:'English Premiere League',
-            5:'English Championship',
-            6:'English FA Cup',
-            7:'Spanish La Liga',
-            8:'German Bundesliga',
-            9:'Italian Serie A',
-            10:'French Ligue 1',
-            11:'Major League Soccer',
-            12:'Mexican Liga Bancomer',
-            13:'Argentina 1era Division'
+    }
+});
+
+firebase.database().ref('/categories').child('soccer').set({
+    name:'soccer',
+    href:'./assets/img/icons/categories/soccer/soccer.png',
+    'subcategories': {
+        'Fifa World Cup':{
+            name:'Fifa World Cup',
+            href:'./assets/img/icons/categories/soccer/Fifa_World_Cup/Fifa_World_Cup.png'
+        },
+        'UEFA Championship League':{
+            name:'UEFA Championship League',
+            href:'./assets/img/icons/categories/soccer/UEFA_Championship_League/UEFA_Championship_League.png'
+        },
+        'UEFA Europa League':{
+            name:'UEFA Europa League',
+            href:'./assets/img/icons/categories/soccer/UEFA_Europa_League/UEFA_Europa_League.png'
+        },
+        'English Premiere League':{
+            name:'English Premiere League',
+            href:'./assets/img/icons/categories/soccer/English_Premiere_League/English_Premiere_League.png'
+        },
+        'English Championship':{
+            name:'English Championship',
+            href:'./assets/img/icons/categories/soccer/English_Championship/English_Championship.png'
+        },
+        'English FA Cup':{
+            name:'English FA Cup',
+            href:'./assets/img/icons/categories/soccer/English_FA_Cup/English_FA_Cup.png'
+        },
+        'Spanish La Liga':{
+            name:'Spanish La Liga',
+            href:'./assets/img/icons/categories/soccer/Spanish_La_Liga/Spanish_La_Liga.png'
+        },
+        'German Bundesliga':{
+            name:'German Bundesliga',
+            href:'./assets/img/icons/categories/soccer/German_Bundesliga/German_Bundesliga.png'
+        },
+        'Italian Serie A':{
+            name:'Italian Serie A',
+            href:'./assets/img/icons/categories/soccer/Italian_Serie_A/Italian_Serie_A.png'
+        },
+        'French Ligue 1':{
+            name:'French Ligue 1',
+            href:'./assets/img/icons/categories/soccer/French_Ligue_1/French_Ligue_1.png'
+        },
+        'Major League Soccer':{
+            name:'Major League Soccer',
+            href:'./assets/img/icons/categories/soccer/Major_League_Soccer/Major_League_Soccer.png'
+        },
+        'Mexican Liga Bancomer':{
+            name:'Mexican Liga Bancomer',
+            href:'./assets/img/icons/categories/soccer/Mexican_Liga_Bancomer/Mexican_Liga_Bancomer.png'
+        },
+        'Argentina 1era Division':{
+            name:'Argentina 1era Division',
+            href:'./assets/img/icons/categories/soccer/Argentina_1era_Division/Argentina_1era_Division.png'
         }
-    });
+    }
+});
